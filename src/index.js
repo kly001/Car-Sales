5336b26918dc0e30434dd739150e79537c4d7417
kly001 <karenly1961@gmail.com>
2019-09-25 20:26:31 -0400

    Edited src/index: createStore,Provider,carReducer

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const store = createStore(carReducer)
+
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>,
+    rootElement);
