44c4117fe42944b5fd3aa3fd654e21c0e25fba33
kly001 <karenly1961@gmail.com>
2019-09-26 23:29:45 -0400

    Edited onClicks

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {removeFeature} from "../store/actions"
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" onClick = {removeFeature}>X</button>
+      <button 
+        className="button"
+        onClick={() => props.removeItem(props.feature)}>
+          X
+      </button>
       {props.feature.name}
     </li>
   );
